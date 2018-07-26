0c3181e0e932ba8c3644b427865db75d025a7267
Unknown <oms1226@gmail.com>
2018-07-26 22:19:33 +0900

    patch

--- python_examples/trtc_train_object_detector.py
+++ python_examples/trtc_train_object_detector.py
@@ -25,8 +25,21 @@ def mkdirs(fullpathName):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
-def getFolderName4TraingOptions():
-    pass
+def getFolderName4TraingOptions(options):
+    reVal = None
+    for key in options.keys():
+        if reVal != None:
+            reVal = reVal + '.'
+        if '-t' == key:
+            options[key] = '/'.join(str(options[key]).split('/')[:-1])
+        newKey = key.replace('-', '_').replace('/', '.').replace('\\', '.')
+        value = str(options[key]).replace('-', '_').replace('/', '.').replace('\\', '.')
+        if reVal == None:
+            reVal = ("%s[%s]" % (newKey, value))
+        else:
+            reVal = reVal + ("%s[%s]" % (newKey, str(value)))
+
+    return reVal + '_' + (datetime.datetime.utcnow() + datetime.timedelta(hours=9)).strftime("%Y%m%d%H%M")
 
 class trtc_train_object_detector4SVM ():
     mTrainerFullPath = None
@@ -49,6 +62,8 @@ class trtc_train_object_detector4SVM ():
         pass
 
 if __name__ == "__main__":
+    traing_options = {'-t': "../trainingDatas/20180423/1_detector_front.xml", '--u': 3, '--l': 1, '--eps': 0.05, '--p': 0, '--target-size': 6400, '--c': 700, '--n': 9, '--cell-size': 8, '--threshold': 0.15, '--threads': 8}
+    print getFolderName4TraingOptions(traing_options)
     pass
 
 
